3b6fa8a11323433f0e093bb2e90337d158e8cb09
Viacheslav Starikov <slavicstar@gmail.com>
2018-09-12 19:13:03 +0300

    Add stupid closing icon. Just X letter.

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM nginx
-MAINTAINER Vacheslav Starikov <slavicstar@fastmail.com>
-RUN sed -i.bak "s/\([[:space:]]\{1,\}sendfile[[:space:]]\{1,\}\)on/\1off/" /etc/nginx/nginx.conf
-COPY . /usr/share/nginx/html
-VOLUME /usr/share/nginx/html
-
--- jquery.stupidbox.js
+++ jquery.stupidbox.js
@@ -1,37 +1,39 @@
 (function ($) {
-    $.fn.stupidbox = function () {
-        var b = $("body"),
-            name = "stupidbox",
-            cls = "." + name,
-            t = '<a href="$" id="' + name + '" style="' +
-                    'display:block;' +
-                    'position:fixed;' +
-                    'top:0;' +
-                    'right:0;' +
-                    'bottom:0;' +
-                    'left:0;' + 
-                    'background:url($) center center rgba(0,0,0,.8) no-repeat;' +
-                    'background-size:contain;' +
-                    'z-index:9999999;' +
-                '"></a>';
+  $.fn.stupidbox = function () {
+    var b = $("body");
+    var name = "stupidbox";
+    var cls = "." + name;
+    var t = 
+      '<a href="$" id="' + name + '" style="' +
+      'display:block;' +
+      'position:fixed;' +
+      'top:0;' +
+      'right:0;' +
+      'bottom:0;' +
+      'left:0;' + 
+      'background:url($) center center rgba(0,0,0,.8) no-repeat;' +
+      'background-size:contain;' +
+      'z-index:9999999">' +
+      '<b style="position:fixed;top:20px;right:20px;z-index:99999999;color:#fff;font-size:50px;font-family:sans-serif">X</b>' + 
+      '</a>';
 
-        b.off(cls).on("click" + cls, cls, function (e) {
-            var el = $(this),
-                href = el.attr("href"),
-                src = el.attr("src"),
-                url = href || src;
+    b.off(cls).on("click" + cls, cls, function (e) {
+      var el = $(this);
+      var href = el.attr("href");
+      var src = el.attr("src");
+      var url = href || src;
 
-            if (url) {
-                $(t.replace(/\$/g, url)).insertAfter(b).on("click", function (e) {
-                    e.preventDefault();
-                    $(this).remove();
-                });
-            }
-
-            e.preventDefault();
+      if (url) {
+        $(t.replace(/\$/g, url)).insertAfter(b).on("click", function (e) {
+          e.preventDefault();
+          $(this).remove();
         });
+      }
 
-        return this.addClass(name);
-    };
+      e.preventDefault();
+    });
+
+    return this.addClass(name);
+  };
 })(jQuery);
 
